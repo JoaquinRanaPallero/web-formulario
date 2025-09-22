0f65575ce65b9b96d90ebf4e597117a46b1f72e8
joaquin-THINKPAD-590 <joaquintomasranapallero@gmail.com>
2025-09-22 12:56:42 -0300

    avances varios

--- web-formulario/Default.aspx.cs
+++ web-formulario/Default.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using dominio; // ¡No olvides agregar esta línea!
 
 namespace web_formulario
 {
@@ -11,7 +12,22 @@ namespace web_formulario
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            // Usamos !IsPostBack para que los datos se carguen solo la primera vez que se visita la página,
+            // y no cada vez que se presiona un botón (postback).
+            if (!IsPostBack)
+            {
+                // 1. Creamos una lista de objetos "Auto" para mostrar.
+                List<Auto> lista = new List<Auto>();
+                lista.Add(new Auto { Modelo = "Corolla", Color = "Rojo", Usado = false, Importado = true });
+                lista.Add(new Auto { Modelo = "Focus", Color = "Azul", Usado = true, Importado = false });
+                lista.Add(new Auto { Modelo = "Cruze", Color = "Blanco", Usado = false, Importado = true });
 
+                // 2. Asignamos la lista de autos como la fuente de datos de la grilla.
+                dgvAutos.DataSource = lista;
+
+                // 3. Enlazamos los datos a la grilla para que se muestren.
+                dgvAutos.DataBind();
+            }
         }
     }
 }
